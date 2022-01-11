5baac30ee23c1f5dcfec384e573cc4ad745e2fdb
Marine Grandcolas <marine-grandcolas_student2021@wilder.school>
2022-01-11 17:31:06 +0100

    API fetch ok and cards generated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import FirstFilter from './components/FirstFilter'
+import GetCards from './components/GetCards';
+import NoResult from './assets/NoResult.png';
+import React from "react";
+import SearchBar from './components/SearchBar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome web-chat-pedia!</h1>
+    <div>
+      <div className="title-and-searchbar">
+      <h1>Wikipawdia</h1>
+      <SearchBar/>
+      <FirstFilter/>
+      <GetCards />
     </div>
-  );
+    </div>
+  )
 }
 
 export default App;
