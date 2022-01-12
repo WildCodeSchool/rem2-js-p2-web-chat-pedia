06fe6435943e5c195deaa34e10e58948c5c8fa46
Romain JUGAL <romain-jugal_student2021@wilder.school>
2022-01-12 16:22:13 +0100

    create SearchBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome web-chat-pedia!</h1>
+      <SearchBar />
     </div>
   );
 }
