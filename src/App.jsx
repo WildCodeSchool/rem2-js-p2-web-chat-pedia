9f3624e08559ede2594aacb9250b57796983e750
Juliette Rivat <julietterivat54@gmail.com>
2022-01-13 17:45:36 +0100

    scrollToTopFinish

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import React from "react";
 import './App.css';
-import ScrollToTop from "./components/ScrollToTop";
+import Scroll  from "./components/Scroll";
 
 function App() {
   
   return (
     <div className="App">
-      <ScrollToTop showBelow={250}/>
-      <h1>Welcome web-chat-pedia!</h1>
-      <div className="container">
-        <div className="box box--1"></div>
-        <div className="box box--2"></div>
-        <div className="box box--3"></div>
-        <div className="box box--4"></div>
-        <div className="box box--5"></div>
-      </div>
-
+      <Scroll/>
     </div>
   );
 }
