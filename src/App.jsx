1f372d078cccbb4f23b8a535036bbb276f9a03d3
Florent-Lefevre <lefevre.flo@gmail.com>
2022-01-13 17:55:40 +0100

    Navbar s01

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { Routes, Route } from 'react-router-dom';
+
+import NavBar from './components/navbar/NavBar'
+import Deposit from './pages/Deposit';
+import Home from './pages/Home';
+import Login from './pages/Login';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome web-chat-pedia!</h1>
+
+      <header className="App-header">
+        <NavBar/>
+        <Routes>
+          <Route exact path="/" element={< Home />} />
+          <Route path="depot" element={< Deposit />} />
+          <Route path="login" element={< Login />} />
+        </Routes>
+      </header>
     </div>
   );
 }
---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,34 @@
+import { NavLink } from 'react-router-dom';
+import logo from '../../assets/logo.png'
+import { useState } from 'react';
+import "./NavBar.css";
+
+const Navbar = () => {
+
+    const [isVisible, setIsVisible] = useState(false)
+
+    return (
+        <div >
+            <div className='topline'></div>
+            <div className='Navbar'>
+                
+                <NavLink to="/"><img src={logo} alt="cat logo" className='logo'/></ NavLink>
+                <NavLink to="/"className="title-container"><p className="title">Welcome web-chat-pedia!</p></ NavLink>
+
+                
+                <input type="text" className={isVisible ? "search-bar-on" : "search-bar"} />
+
+
+                <div className='navigation'>
+                    <div className="search-button" onClick={() => setIsVisible(!isVisible)}><span class="material-icons">search</span></div>
+                    <NavLink to="/" className={(navData) => (navData.isActive ? "navigation-link selected" : "navigation-link")}><i className="material-icons">home</i></ NavLink>
+                    <NavLink to="depot" className={(navData) => (navData.isActive ? "navigation-link selected" : "navigation-link")}><i className="material-icons">cloud_upload</i></ NavLink>
+                    <NavLink to="login" className={(navData) => (navData.isActive ? "navigation-link selected" : "navigation-link")}><i className="material-icons">login</i></ NavLink>
+                </div>
+            </div>
+        
+        </div>
+    )
+}
+
+export default Navbar
