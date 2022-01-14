638bac6f7347d6ad31b5f424b088a077a48641ec
Marine Grandcolas <marine-grandcolas_student2021@wilder.school>
2022-01-14 10:34:48 +0100

    update cards

---
+++ src/components/GetCards.js
@@ -0,0 +1,30 @@
+import {useEffect, useState} from 'react';
+import GenerateCard from './GenerateCard';
+import "./GetCards.css"
+
+function GetCards() {
+  const [cards, setCards] = useState([]);
+  
+  useEffect(()=> {
+    fetch("https://a.nacapi.com/AssetsDevList")
+    .then(response => response.json())
+    .then(data => setCards(data))
+    .catch(console.error)
+  }, [] );
+
+  return (
+    <div className="GetCard">
+      {
+      cards &&
+      cards.map((card) => (
+        <div key= {card.id}>
+          <GenerateCard card={card}/>
+        </div>
+      ))
+      }
+        
+    </div>
+  );
+}
+
+export default GetCards;
