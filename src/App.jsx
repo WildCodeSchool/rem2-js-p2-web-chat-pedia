21ab8a147b69fc1c808c62d729e3788bc893e28b
YacineHATEM <yacine.hatem@gmail.com>
2022-01-14 11:29:09 +0100

    footers01

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome web-chat-pedia!</h1>
+     <Footer/>
     </div>
   );
 }
