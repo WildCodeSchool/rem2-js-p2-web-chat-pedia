1aec936d8ea21e8188ce0bcafdafb4588c6577b6
Florent-Lefevre <lefevre.flo@gmail.com>
2022-01-14 09:19:37 +0100

    navbar comment delete

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -5,7 +5,7 @@ import "./NavBar.css";
 
 const Navbar = () => {
 
-    const [isVisible, setIsVisible] = useState(false)
+
 
     return (
         <div >
@@ -13,14 +13,8 @@ const Navbar = () => {
             <div className='Navbar'>
                 
                 <NavLink to="/"><img src={logo} alt="cat logo" className='logo'/></ NavLink>
-                <NavLink to="/"className="title-container"><p className="title">Wikipawdia</p></ NavLink>
-
-                
-                {/* <input type="text" className={isVisible ? "search-bar-on" : "search-bar"} /> */}
-
-
+                <NavLink to="/"className="title-container"><p className="title">Wikipawdia!</p></ NavLink>
                 <div className='navigation'>
-                    <div className="search-button" onClick={() => setIsVisible(!isVisible)}><span class="material-icons">search</span></div>
                     <NavLink to="/" className={(navData) => (navData.isActive ? "navigation-link selected" : "navigation-link")}><i className="material-icons">home</i></ NavLink>
                     <NavLink to="depot" className={(navData) => (navData.isActive ? "navigation-link selected" : "navigation-link")}><i className="material-icons">cloud_upload</i></ NavLink>
                     <NavLink to="login" className={(navData) => (navData.isActive ? "navigation-link selected" : "navigation-link")}><i className="material-icons">login</i></ NavLink>
